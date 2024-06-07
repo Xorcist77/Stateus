3f8ea8594b2003d9749d31058fde81dbee7f0fd4
Xorcist <Xorcist@Comcast.net>
2024-06-07 17:09:11 -0400

    Generate Single File EXE
    Added Costura.Fody Nuget package to embed System.CommandLine assemblies into a single file EXE.

--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ using System.Linq;
 using System.Management;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Runtime.Remoting.Contexts;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
